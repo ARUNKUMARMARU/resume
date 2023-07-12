9c875d44ed8a004c5b3405140737d6b52298b1f0
ARUNKUMARMARU <arunsmart1020@gmail.com>
2023-07-12 21:17:13 +0530

    resume

---
+++ resume.js
@@ -0,0 +1,61 @@
+let myResume={
+    "basics": {
+      "name": "ARUNKUMAR M",
+      "email": "arunsmart1020@gamil.com",
+      "phone": 7397658644,
+      "degree": "B.E",
+      "location": {
+        "address": "7/175,Keelkadhavukarai,K.G.Palayam(po),Annur.",
+        "postalCode": 641697,
+        "city": "Coimbatore",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          "website": "https://www.linkedin.com/in/arun-kumar-665a1917b/",
+          "github":"https://github.com/ARUNKUMARMARU"
+        }
+      ]
+    },
+    "work": [
+      {
+        "company": "BEST HOME DEVELOPERS",
+        "position": "Civil Engineer",
+        "startDate": "26-02-2020",
+        "endDate": "till date",
+        "summary": "i am the sitesupervising, customer executive and gaining lots of experience",
+      },
+    ],
+    "education": [
+      {
+        "institution": "SNS College of Technology",
+        "department": "Civil",
+        "studyType": "fulltime",
+        "batch start year": 2016,
+        "batch end year": 2020,
+        "gpa": 8.5,
+      }
+    ],
+    "skills": [
+      {
+        "name": "javascript,html",
+        "level": "beginer",
+        "project": [
+          "nill"
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,Enlish",
+      }
+    ],
+    "interests": [
+      {
+        "name": "script writter,",
+      }
+    ]
+  }
+  console.log(myResume);
+  
